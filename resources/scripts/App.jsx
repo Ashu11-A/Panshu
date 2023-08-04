c8c3f2c061586bdfb84405fe5e3e6527e135478e
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-08-04 17:55:07 -0400

    Testando o bizcharts (Perfeito)

--- resources/scripts/App.jsx
+++ resources/scripts/App.jsx
@@ -51,31 +51,35 @@ const App = () => {
                 <Loading />
             ) : (
                 <>
-                    <ContentBox>
-                    <LineChart APIs={website} />
-                        <img
-                            src='/vite.svg'
-                            alt="Ícone do React"
-                            className="w-16 h-16 mx-auto mb-4"
-                        />
-                        <h1 className="text-4xl font-bold text-blue-600 mb-4">
-                            Bem Vindo ao Começo da dor de cabeça chamado React :3
-                        </h1>
-                        <p className="text-gray-600 mb-6">
-                            É aqui que tudo começa.
-                        </p>
-                        <p>Seu Config Atual</p>
-                        <pre className="text-left bg-gray-100 p-4 rounded-md overflow-auto max-h-96">
-                            <code className="text-sm text-gray-800">
-                                {jsonViewer(website)}
-                            </code>
-                        </pre>
-                        <div className='flex justify-center items-center'>
-                            <button onClick={incrementCount} className="bg-green-400 h-[20px] w-[20px] m-3 rounded-2xl">+</button>
-                            {count}
-                            <button onClick={removeCount} className="bg-red-500 h-[20px] w-[20px] m-3 rounded-2xl">-</button>
-                        </div>
-                    </ContentBox>
+                    <div className='grid grid-cols-3'>
+                        <ContentBox>
+                            <LineChart data={website.hourlyData} />
+                        </ContentBox>
+                        <ContentBox>
+                            <img
+                                src='/vite.svg'
+                                alt="Ícone do React"
+                                className="w-16 h-16 mx-auto mb-4"
+                            />
+                            <h1 className="text-4xl font-bold text-blue-600 mb-4">
+                                Bem Vindo ao Começo da dor de cabeça chamado React :3
+                            </h1>
+                            <p className="text-gray-600 mb-6">
+                                É aqui que tudo começa.
+                            </p>
+                            <p>Seu Config Atual</p>
+                            <pre className="text-left bg-gray-100 p-4 rounded-md overflow-auto max-h-96">
+                                <code className="text-sm text-gray-800">
+                                    {jsonViewer(website)}
+                                </code>
+                            </pre>
+                            <div className='flex justify-center items-center'>
+                                <button onClick={incrementCount} className="bg-green-400 h-[20px] w-[20px] m-3 rounded-2xl">+</button>
+                                {count}
+                                <button onClick={removeCount} className="bg-red-500 h-[20px] w-[20px] m-3 rounded-2xl">-</button>
+                            </div>
+                        </ContentBox>
+                    </div>
                 </>
             )}
         </div>
